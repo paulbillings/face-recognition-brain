96abbf7b6701c155bab0a9f9a15a4291c91da58b
Paul Billings <paulbillings101@yahoo.co.uk>
2019-09-07 15:06:18 +0100

    added logo with tilt.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import Navigation from "./components/Navigation/Navigation";
+import Logo from "./components/Logo/Logo";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Navigation />
-      {/*<Logo />
+      <Logo />
+      {/*
       <ImageLinkForm />
-      <FaceRecognition />*/}
+      <FaceRecognition />
+    */}
     </div>
   );
 }
