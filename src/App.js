899adad2183b37be79df9bc7112d73e56815898a
Paul Billings <paulbillings101@yahoo.co.uk>
2019-09-13 17:12:35 +0100

    added register component and routing

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import Rank from "./components/Rank/Rank";
 import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
 import Signin from "./components/Signin/Signin";
+import Register from "./components/Register/Register";
 import Particles from 'react-particles-js';
 import './App.css';
 
@@ -80,9 +81,8 @@ class App extends Component {
             params={particlesOptions}
          />
         <Navigation onRouteChange={this.onRouteChange}/>
-        { this.state.route === "signin" 
-          ? <Signin onRouteChange={this.onRouteChange}/>
-          : <div>
+        { this.state.route === "home" 
+          ? <div>
               <Logo />
               <Rank />
               <ImageLinkForm 
@@ -91,6 +91,12 @@ class App extends Component {
               />
               <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
             </div>
+          : (
+              this.state.route === "signin" 
+              ? <Signin onRouteChange={this.onRouteChange}/>
+              : <Register onRouteChange={this.onRouteChange}/>
+            )
+          
         }
       </div>
     );
