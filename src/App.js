cfecece47fb477b844413ba440c8a833fa4a7dab
Paul Billings <paulbillings101@yahoo.co.uk>
2019-09-22 21:00:22 +0100

    updated the rank component to display user name and number of entries

--- src/App.js
+++ src/App.js
@@ -83,12 +83,27 @@ class App extends Component {
     app.models.predict(
       Clarifai.FACE_DETECT_MODEL,
       this.state.input)
-    .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+    .then(response => {
+      if (response) {
+        fetch("http://localhost:3001/image", {
+          method: "put",
+          headers: {"Content-Type": "application/json"},
+          body: JSON.stringify({
+            id: this.state.user.id           
+          })
+        })
+        .then(response => response.json())
+        .then(count => {
+          this.setState(Object.assign(this.state.user, { entries: count }))
+        })
+      }
+      this.displayFaceBox(this.calculateFaceLocation(response))
+    })
     .catch(err => console.log(err));
   }
 
   onRouteChange = (route) => {
-    if (route === "signin") {
+    if (route === "signout") {
       this.setState({isSignedIn: false})
     } else if (route === "home") {
       this.setState({isSignedIn: true})
@@ -107,7 +122,7 @@ class App extends Component {
         { route === "home" 
           ? <div>
               <Logo />
-              <Rank />
+              <Rank name={this.state.user.name} entries={this.state.user.entries}/>
               <ImageLinkForm 
                 onInputChange={this.onInputChange} 
                 onButtonSubmit={this.onButtonSubmit}
@@ -116,7 +131,7 @@ class App extends Component {
             </div>
           : (
               route === "signin" 
-              ? <Signin onRouteChange={this.onRouteChange}/>
+              ? <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
               : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
             )
           
