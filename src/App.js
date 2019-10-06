63480e8b32dc971d94b1f77783cd78c78216d0eb
Paul Billings <paulbillings101@yahoo.co.uk>
2019-10-06 16:16:02 +0100

    moved face recognition api call to the backend

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Navigation from "./components/Navigation/Navigation";
-import Clarifai from 'clarifai';
 import Logo from "./components/Logo/Logo";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import Rank from "./components/Rank/Rank";
@@ -22,11 +21,6 @@ const particlesOptions = {
     }
   }
 } 
-
-const app = new Clarifai.App({
- apiKey: '60aee873e4f14469a32c2771e1dd7e0a'
-});
-
 
 const initialState = {
   input: "",
@@ -82,9 +76,14 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input})
-    app.models.predict(
-      Clarifai.FACE_DETECT_MODEL,
-      this.state.input)
+    fetch("http://localhost:3001/imageurl", {
+          method: "post",
+          headers: {"Content-Type": "application/json"},
+          body: JSON.stringify({
+            input: this.state.input           
+          })
+    })
+    .then(response => response.json())
     .then(response => {
       if (response) {
         fetch("http://localhost:3001/image", {
